6bb7f19aed63246147d1180d89f7a035d3720db7
Stuart428 <199536630+Stuart428@users.noreply.github.com>
2026-05-15 14:26:52 +1000

    awdWD

--- src/App.tsx
+++ src/App.tsx
@@ -20,18 +20,21 @@ async function generateMlKemKeyPair()
   const [publicKey, secretKey] = keypair.generateKeyPair();
   return { publicKey, secretKey };
 }
+
 async function generateSymKeyAndEncryptMlKem(publicKey: Uint8Array)
 {
   const sender = await createMlKem1024();
   const [cipherText, sharedSymmetricSecret] = sender.encap(publicKey);
   return { cipherText, sharedSymmetricSecret };
 }
+
 async function decryptMlKem(cipherText: Uint8Array, secretKey: Uint8Array)
 {
   const receiver = await createMlKem1024();
   const sharedSymmetricSecret = receiver.decap(cipherText, secretKey);
   return sharedSymmetricSecret;
 }
+
 function encryptWithSymmetricKeyBase64 (plainText: string, symmetricKey: Uint8Array) 
 {
   const initializationVector = Buffer.from(crypto.randomBytes(12), 'utf8');
@@ -41,6 +44,7 @@ function encryptWithSymmetricKeyBase64 (plainText: string, symmetricKey: Uint8Ar
   enc += cipher.final('base64');
   return [enc, initializationVector, cipher.getAuthTag()];
 }
+
 function decryptWithSymmetricKeyBase64(enc:string, initializationVector: Buffer, authTag: Buffer, symmetricKey: Uint8Array) 
 {
   const decipher = crypto.createDecipheriv('aes-256-gcm', Buffer.from(symmetricKey), Buffer.from(initializationVector));
@@ -49,11 +53,15 @@ function decryptWithSymmetricKeyBase64(enc:string, initializationVector: Buffer,
   str += decipher.final('utf8');
   return str;
 }
-async function vgpGenerateKeyPair() {
+
+async function vgpGenerateKeyPair() 
+{
   const { publicKey, secretKey } = await generateMlKemKeyPair();
   return { publicKey, secretKey };
 }
-async function mlKemPlusAesEncrypt(symetricPackage: string, publicKey: Uint8Array) {
+
+async function mlKemPlusAesEncrypt(symetricPackage: string, publicKey: Uint8Array) 
+{
   const { cipherText, sharedSymmetricSecret } = await generateSymKeyAndEncryptMlKem(publicKey);
   const [enc, initializationVector, authTag] = encryptWithSymmetricKeyBase64(symetricPackage, sharedSymmetricSecret);
   return { cipherText, enc, initializationVector, authTag };
