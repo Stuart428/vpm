679e20775d56da7d14d3db5233e8ef477bfddfab
Stuart <199536630+Stuart428@users.noreply.github.com>
2026-05-28 11:26:40 +1000

    fixed crypto poly fill hopfully

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills()
+  ],
+
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      buffer: 'buffer',
+      process: 'process/browser'
+    }
+  },
+
+  define: {
+    global: 'globalThis'
+  }
 })
