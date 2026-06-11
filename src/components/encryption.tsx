3e11410ba56482603c0b0fd87d5800fe8b281d80
Stuart <199536630+Stuart428@users.noreply.github.com>
2026-06-11 13:56:24 +1000

    aawdwddwd

--- src/components/encryption.tsx
+++ src/components/encryption.tsx
@@ -14,7 +14,7 @@ async function onSumbit(e: React.FormEvent<HTMLFormElement>, publicKeyIn: string
     console.log(publicKeyIn);
     const message = messageIn;
     const symetricPackage : symmetricPackage = {message: message, filePackage: files };
-    alert(files);
+    alert(JSON.stringify(files));
     const encryptedPackage : encryptedPackage = await vgpEncrypt(symetricPackage, publicKey);
     const safePackage = {
         encryptedSymmetricKey: Buffer.from(encryptedPackage.encryptedSymmetricKey).toString("base64"),
