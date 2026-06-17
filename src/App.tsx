e98264bdfc119c91ed7d7e3695f642c8b983cf33
Stuart <199536630+Stuart428@users.noreply.github.com>
2026-06-17 11:42:10 +1000

    added names

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,20 @@ function App() {
 
   return (
     <>
-      <div> <button onClick={() => {if (showKeygen == true){setShowKeygen(false);} else {setShowKeygen(true);}}}>Toggle visibility</button></div>
+      <div> 
+        <h2><Keygen></Keygen></h2>
+        <button onClick={() => {if (showKeygen == true){setShowKeygen(false);} else {setShowKeygen(true);}}}>Toggle visibility</button>
+      </div>
       {showKeygen && <KeyGen />}
-      <div> <button onClick={() => {if (showEncryption == true){setShowEncryption(false);} else {setShowEncryption(true);}}}>Toggle visibility</button></div>
+      <div> 
+        <h2>Encryption</h2>
+        <button onClick={() => {if (showEncryption == true){setShowEncryption(false);} else {setShowEncryption(true);}}}>Toggle visibility</button>
+      </div>
       {showEncryption && <Encryption />}
-      <div> <button onClick={() => {if (showDecryption == true){setShowDecryption(false);} else {setShowDecryption(true);}}}>Toggle visibility</button></div>
+      <div> 
+        <h2>Decryption</h2>
+        <button onClick={() => {if (showDecryption == true){setShowDecryption(false);} else {setShowDecryption(true);}}}>Toggle visibility</button>
+      </div>
       {showDecryption && <Decryption />}
     </>
   )
