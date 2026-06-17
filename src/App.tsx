e00aa2905ea913c34f418baa23ccfec854e3dcd2
Stuart <199536630+Stuart428@users.noreply.github.com>
2026-06-17 11:33:03 +1000

    jeremy

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import './App.css'
 import Encryption from './components/encryption';
 import KeyGen from './components/keygen';
 import Decryption from './components/decryption';
+import { useState } from 'react';
 function App() {
-
+  const [showKeygen, setShowKeygen] = useState(true)
+  const [showEncryption, setShowEncryption] = useState(true)
+  const [showDecryption, setShowDecryption] = useState(true)
 
   
   
 
   return (
     <>
-      <KeyGen />
-      <Encryption />
-      <Decryption />
+      {showKeygen && <KeyGen />}
+      {showEncryption && <Encryption />}
+      {showDecryption && <Decryption />}
     </>
   )
 }
